3cfd296872cdf9e403f9c0ff6422a4dfd23a4892
Jan Rosa <102300602+jrosa02@users.noreply.github.com>
2024-03-17 19:36:44 +0100

    aaaa

--- lab03/Uart_transmiter/Uart_transmiter.srcs/sim_1/new/stimulate_transmitter.v
+++ lab03/Uart_transmiter/Uart_transmiter.srcs/sim_1/new/stimulate_transmitter.v
@@ -24,6 +24,7 @@ module stimulate_transmitter
 (
     output clk,
     output rst,
+    output rst2,
     output send,
     output [7:0]data
 );
@@ -32,7 +33,9 @@ integer file;
 reg clk_reg = 0;
 reg [7:0] data_reg = 0;
 reg send_reg = 0;
-reg [7:0] i;
+reg [7:0] i = 0;
+reg rst_reg = 0;
+reg rst2_reg = 0;
 
 initial begin
     clk_reg <= 0; file = $fopen("C:/Users/janro/Pulpit/AGH_FILES/SR-2024S/lab03/simulate_bin.bin", "rb");
@@ -46,12 +49,18 @@ initial begin
     $fclose(file);
 end
 
+initial begin
+    #200; rst_reg = 1;
+    #50; rst2_reg = 1;
+end
+
 always #1 clk_reg = ~clk_reg;
 
 assign send = send_reg;
 assign data = data_reg;
 assign clk = clk_reg;
-assign rst = 0;
+assign rst = rst_reg;
+assign rst2 = rst2_reg;
 
 
 endmodule
--- lab03/python_test.py
+++ lab03/python_test.py
@@ -5,16 +5,12 @@ with open("simulate_bin.bin", "r") as file:
 
 strink = ""
 
-for bin_repr in data:
+for bin_repr in data: #simulate uart
     bin_string = bin(ord(bin_repr))[2:].zfill(8)[::-1]
     strink = "".join([strink, "01", bin_string, "00"])
 
-print(strink[:20])
-
-
-
 with open("txd_bin.bin", "r") as file:
     data_txd = file.read()
-    data2print = data_txd.replace('\n', '')[4:]
+    data2print = data_txd.replace('\n', '')[3:386:2] #values are doubled 
 
-print(data2print[:20])
+print(data2print == strink)
