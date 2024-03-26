488d0566f6e4640d6e537211bebb07c5fdb3ef1b
Jan Rosa <102300602+jrosa02@users.noreply.github.com>
2024-03-26 10:52:51 +0100

    aaa

--- lab04/accum/accum.srcs/sim_1/new/testbench.v
+++ lab04/accum/accum.srcs/sim_1/new/testbench.v
@@ -23,16 +23,28 @@
 module testbench();
 
 reg clk = 0;
-reg ce = 1;
-reg signed [12:0] A = 13'hFFFF;
+reg ce = 0;
+reg signed [12:0] A = 13'h0000;
 reg rst = 0;
 
 wire [20:0] Y;
 
 always #1 clk = ~clk;
 initial #512 rst = 1;
-initial begin
-    ;
+initial begin;
+    #2 ce = 1;
+    #2 A = 13'h18ac;
+    #2 A = 13'h1a58;  
+    #2 A = 13'h1c04;  
+    #2 A = 13'h1db0;  
+    #2 A = 13'h1f5c;  
+    #2 A = 13'h0108;  
+    #2 A = 13'h02b4;  
+    #2 A = 13'h045f;  
+    #2 A = 13'h060b;  
+    #2 A = 13'h07b7;
+    #3 rst = 1;
+    #3 $finish;
 end
 
 accum cumm
